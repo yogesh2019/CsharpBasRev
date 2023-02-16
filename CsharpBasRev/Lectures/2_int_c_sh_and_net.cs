0776d5100c6b5d4ec5bc38b601f39a524d83df3c
yogesh2019 <yogesh.khichi@gmail.com>
2023-02-16 12:19:04 +0530

    Add project files.

---
+++ CsharpBasRev/Lectures/2_int_c_sh_and_net.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasRev.Lectures
+{
+    public class _2_int_c_sh_and_net
+    {
+        public static void _2_intro_to_cs_dot()
+        {
+           
+            Console.WriteLine(" C# vs .Net\r\n            CLR\r\n            Architecture of .Net applications => we will see constants namespaces and assemblies\r\n            First C# application");
+        }
+        public static void _3_c_sharp_vs_dot_net()
+        {
+
+            Console.WriteLine("C sharp is a prog language");
+        }
+    }
+}
---
+++ CsharpBasRev/Program.cs
@@ -0,0 +1,19 @@
+﻿using CsharpBasRev.Lectures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasRev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _2_int_c_sh_and_net._2_intro_to_cs_dot();
+
+            Console.ReadLine();
+        }
+    }
+}
